f5a60d4a92c0b1c136e9ab485788d4cdb9246651
Nasos Kouroupis <hello@nasoku.com>
2017-02-22 22:00:57 +0200

    css update

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,14 @@
-$(".menu-toggle").on('click', function() {
-  $(this).toggleClass("on");
-  $('.menu-section').toggleClass("on");
-  $("nav ul").toggleClass('hidden');
-});
+function menu() {
+  var $toggle = document.getElementById('menu_toggle');
+  var $sideMenu = document.getElementById('side-menu');
+  var $close = document.getElementById('close');
+  var $tl       = new TimelineMax();
+  $toggle.addEventListener('click', function(){
+    classie.toggle($sideMenu, "sideMenuToggle");
+    $tl.staggerFrom('li', .5, {alpha:0, xPercent:"-=50", ease:Back.easeOut},0.2);
+  });
+  $close.addEventListener('click', function(){
+    classie.remove($sideMenu, "sideMenuToggle");
+  });
+}
+menu();
