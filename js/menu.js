bbb1ee1304b78345c85866e99bbcb7093613f5a6
Kristoffer Aarholt <kaarholt@gmail.com>
2017-03-16 09:33:44 +0100

    Added toggling for menu and display divs

--- js/menu.js
+++ js/menu.js
@@ -1,14 +1,18 @@
 function menu() {
-  var $toggle = document.getElementById('menu_toggle');
-  var $sideMenu = document.getElementById('side-menu');
-  var $close = document.getElementById('close');
-  var $tl       = new TimelineMax();
-  $toggle.addEventListener('click', function(){
-    classie.toggle($sideMenu, "sideMenuToggle");
-    $tl.staggerFrom('li', .5, {alpha:0, xPercent:"-=50", ease:Back.easeOut},0.2);
+  // var $toggle = document.getElementById('menu_toggle');
+  // var $sideMenu = document.getElementById('side-menu');
+  var $menuCloseButton = document.getElementById('menuCloseButton');
+  var $menuOpenButton = document.getElementById('menuOpenButton');
+
+  // var $tl       = new TimelineMax();
+  $menuOpenButton.addEventListener('click', function() {
+    document.body.classList.add('is-showing-menu');
+    // classie.toggle($sideMenu, "sideMenuToggle");
+    // $tl.staggerFrom('li', .5, {alpha:0, xPercent:"-=50", ease:Back.easeOut},0.2);
   });
-  $close.addEventListener('click', function(){
-    classie.remove($sideMenu, "sideMenuToggle");
+  $menuCloseButton.addEventListener('click', function() {
+    // classie.remove($sideMenu, "sideMenuToggle");
+    document.body.classList.remove('is-showing-menu');
   });
 }
 menu();
